2d1bfbacdfcf749a0a1411b4cdcb4d738e32bfc6
SBEGENJOV <seydi19981995@gmail.com>
2023-09-09 13:57:46 +0300

    09.09.2023 14:00

---
+++ Web_Calısmaları/mvc_1/razor_2/razor_2/Controllers/HomePage2Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace razor_2.Controllers
+{
+    public class HomePage2Controller : Controller
+    {
+        // GET: HomePage2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+    }
+}
