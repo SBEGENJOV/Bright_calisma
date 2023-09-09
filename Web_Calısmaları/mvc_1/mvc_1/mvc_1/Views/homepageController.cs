6d161e61193bbbe6a472c3a8523bda4bbb3fa577
SBEGENJOV <seydi19981995@gmail.com>
2023-09-09 12:55:35 +0300

    Razor kullanım

---
+++ Web_Calısmaları/mvc_1/mvc_1/mvc_1/Views/homepageController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_1.Views
+{
+    public class homepageController : Controller
+    {
+        // GET: homepage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
