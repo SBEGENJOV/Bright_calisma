83145674512f87e045d5f89b810293484520f763
SBEGENJOV <seydi19981995@gmail.com>
2023-09-15 10:56:32 +0300

    14.09.2023 22:00

--- Web_Calısmaları/web_model_firs/model_first/model_first/Controllers/PersonelController.cs
+++ Web_Calısmaları/web_model_firs/model_first/model_first/Controllers/PersonelController.cs
@@ -16,5 +16,61 @@ namespace model_first.Controllers
             var result= db.PersonelSet.ToList();
             return View(result);
         }
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeni(Personel personel)
+        {
+            try
+            {
+                using(Model1Container con=new Model1Container())
+                {
+                    con.PersonelSet.Add(personel);
+                    con.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var result = db.PersonelSet.Where(x => x.PersonelNo == id).FirstOrDefault();
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Personel personel)
+        {
+            try
+            {
+                db.Entry(personel).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var result = db.PersonelSet.Where(x => x.PersonelNo == id).FirstOrDefault();
+            return View(result);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id,Personel personel)
+        {
+            personel=db.PersonelSet.Where(x=>x.PersonelNo==id).FirstOrDefault();
+            db.PersonelSet.Remove(personel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
