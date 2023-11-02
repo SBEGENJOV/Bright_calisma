2de9dbc8401f1efed48db45cdad3cb8b359b862c
SBEGENJOV <seydi19981995@gmail.com>
2023-11-02 15:13:07 +0300

    02.11.2023

---
+++ JS ornekler/notProject/notProject/script.js
@@ -0,0 +1,10 @@
+﻿const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelector("input-box");
+
+
+function showNot() {
+
+    notesContainer.innerHTML = localStorage.getItem("notes");
+
+}
